23bd8da9381d5279030b4df5826a935e6f309425
James-Leslie <jlesl09@gmail.com>
2018-10-23 14:29:28 +0200

    added images and jupyter notebook

--- tests/test.py
+++ tests/test.py
@@ -5,4 +5,6 @@ def test_top_n():
     make sure top_n works correctly
     """
 
-    assert myModule.top_n([8, 3, 2, 7, 4], 3) == [8, 7, 4], 'incorrect'
+    assert myFunction.top_n([8, 3, 2, 7, 4], 3) == [8, 7, 4], 'incorrect'
+    assert myFunction.top_n([10, 1, 12, 9, 2], 2) == [12, 10], 'incorrect'
+    assert myFunction.top_n([1, 2, 3, 4, 5], 5) == [5, 4, 3, 2, 1], 'incorrect'
